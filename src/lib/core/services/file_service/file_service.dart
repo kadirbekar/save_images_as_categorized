9253d2270fd0a6b76ce3459a9dead2182584186f
kadirbekar <kadir.bekr@gmail.com>
2021-10-17 23:52:56 +0300

    try catch blocks added to file operations.

--- src/lib/core/services/file_service/file_service.dart
+++ src/lib/core/services/file_service/file_service.dart
@@ -14,11 +14,14 @@ class FileService {
     required String rootFolderName,
     required List<CategoryModel?>? categoryList,
   }) async {
-    _appDocumentDirectory = await _getPlatformSpecificDirectory();
-    
-    final Directory _rootFolder = Directory("${_appDocumentDirectory?.path}/$rootFolderName");
-    if (!await _rootFolder.exists()) {
-      await _rootFolder.create(recursive: true);
+    try {
+      _appDocumentDirectory = await _getPlatformSpecificDirectory();
+      final Directory _rootFolder = Directory("${_appDocumentDirectory?.path}/$rootFolderName");
+      if (!await _rootFolder.exists()) {
+        await _rootFolder.create(recursive: true);
+      }
+    } catch (e) {
+      Exception(e);
     }
     _createPerImageCategoryFolder(
       categoryList: categoryList,
@@ -37,18 +40,22 @@ class FileService {
   }) {
     categoryList?.forEach(
       (category) async {
-        final String _imageCategoryFolderPath = "${_appDocumentDirectory?.path}/$rootFolderName/${category?.id}";
-        final Directory _categoryFolderName = Directory(_imageCategoryFolderPath);
-        if (!await _categoryFolderName.exists()) {
-          await _categoryFolderName.create(recursive: true);
+        try {
+          final String _imageCategoryFolderPath = "${_appDocumentDirectory?.path}/$rootFolderName/${category?.id}";
+          final Directory _categoryFolderName = Directory(_imageCategoryFolderPath);
+          if (!await _categoryFolderName.exists()) {
+            await _categoryFolderName.create(recursive: true);
+          }
+          category?.images?.forEach((image) async {
+            if(image.path == null) return;
+            await _copyImageToItsRelatedCategoryFolder(
+              path: image.path,
+              folderDirectory: _categoryFolderName.path
+            );
+          });
+        } catch (e) {
+          Exception(e);
         }
-        category?.images?.forEach((image) async {
-          if(image.path == null) return;
-          await _copyImageToItsRelatedCategoryFolder(
-            path: image.path,
-            folderDirectory: _categoryFolderName.path
-          );
-        });
       },
     );
   }
@@ -57,10 +64,14 @@ class FileService {
     required String? path, 
     required folderDirectory
   }) async {
-    const _applicationDataFolderDirectory = "/data/user/0/com.example.save_images_as_categorized/cache/";
-    final File _imageFile = File(path!);
-    final String _imageName = _imageFile.toString().split(_applicationDataFolderDirectory)[1].split("'")[0];
-    await _imageFile.copy("$folderDirectory/$_imageName");
+    try {
+      const _applicationDataFolderDirectory = "/data/user/0/com.example.save_images_as_categorized/cache/";
+      final File _imageFile = File(path!);
+      final String _imageName = _imageFile.toString().split(_applicationDataFolderDirectory)[1].split("'")[0];
+      await _imageFile.copy("$folderDirectory/$_imageName");
+    } catch (e) {
+      Exception(e);
+    }
   }
 
   FileService._initialize();
